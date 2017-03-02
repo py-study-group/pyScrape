f49faf171c0da8ff59c9e79e803ea8f04c3c51bd
Belonias <iakovosbelonias@gmail.com>
2017-03-02 10:55:12 +0200

    comments

--- scrapeScript.py
+++ scrapeScript.py
@@ -1,9 +1,10 @@
 # The urllib.request module defines functions and classes which help in opening URLs
 from urllib.request import urlopen
-
+# The url we want to target
 my_address = "https://realpython.com/practice/aphrodite.html"
+# with urlopen we open the url we want(my_address)
 html_page = urlopen(my_address)
-
+# we store the content of the website
 html_text = html_page.read().decode('utf-8')
-
+# print the content of html_text
 print(html_text)
